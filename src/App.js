cd721a211f36eb44127de6db7939b31633af4f76
ra-res <ra-res@users.noreply.github.com>
2021-01-31 14:07:24 +0000

    CSS and basic background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import React, { useState } from "react";
+
+const api = {
+  key: "73797a4244b51f8fef1013fb9838d80e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <main>
+        <div className='search-box'>
+          <input type='text' className='search-bar' placeholder='Search...' />
+        </div>
+      </main>
     </div>
   );
 }
