77b429acffc862c758490d08dcaec9fd4ea5cc7f
ra-res <ra-res@users.noreply.github.com>
2021-02-01 12:30:38 +0000

    styles app

--- src/App.js
+++ src/App.js
@@ -8,11 +8,57 @@ const api = {
 };
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <div className='app'>
       <main>
         <div className='search-box'>
           <input type='text' className='search-bar' placeholder='Search...' />
+        </div>
+        <div className='location-box'>
+          <div className='location'>New York City, US</div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+        </div>
+
+        <div className='weather-box'>
+          <div className='temp'>15C</div>
+          <div className='weather'>Sunny</div>
         </div>
       </main>
     </div>
