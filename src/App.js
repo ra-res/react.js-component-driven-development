55f07a906c4cf6c7ac7d3dae91dcabf1a13e5b88
ra-res <ra-res@users.noreply.github.com>
2021-02-01 12:57:16 +0000

    Changing background based on weather

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@
 import React, { useState } from "react";
 
 const api = {
-  key: process.env.API_KEY,
+  key: "dbbb6b26fbad0357b4df6128e5ed85ff",
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          // console.log(result);
+          setQuery("");
+        });
+    }
+  };
 
   const dateBuilder = (d) => {
     let months = [
@@ -46,20 +58,42 @@ function App() {
   };
 
   return (
-    <div className='app'>
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }>
       <main>
         <div className='search-box'>
-          <input type='text' className='search-bar' placeholder='Search...' />
+          <input
+            type='text'
+            className='search-bar'
+            placeholder='Search...'
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-        <div className='location-box'>
-          <div className='location'>New York City, US</div>
-          <div className='date'>{dateBuilder(new Date())}</div>
-        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className='location-box'>
+              <div className='location'>
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className='date'>{dateBuilder(new Date())}</div>
+            </div>
 
-        <div className='weather-box'>
-          <div className='temp'>15C</div>
-          <div className='weather'>Sunny</div>
-        </div>
+            <div className='weather-box'>
+              <div className='temp'>{Math.round(weather.main.temp)}*c</div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
