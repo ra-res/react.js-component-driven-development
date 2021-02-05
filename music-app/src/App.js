5fe32f9ff509b6c24622f22005be1b6fab34f8b6
ra-res <ra-res@users.noreply.github.com>
2021-02-05 12:09:25 +0000

    functionality

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,10 +1,10 @@
 /** @format */
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Player from "./Components/Player";
 
 function App() {
-  const [songs, setSongs] = useState([
+  const [songs] = useState([
     {
       title: "Forget me too ft. Halsey",
       artist: "Machine Gun Kelly",
@@ -34,9 +34,24 @@ function App() {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
   return (
     <div className='App'>
-      <Player songs={songs[currentSongIndex]} nextSong={songs[nextSongIndex]} />
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
--- music-app/src/Components/Player.js
+++ music-app/src/Components/Player.js
@@ -1,19 +1,59 @@
 /** @format */
 
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PlayerControls from "./PlayerControls";
 import PlayerDetails from "./PlayerDetails";
 
 function Player(props) {
+  const audioEl = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioEl.current.play();
+    } else {
+      audioEl.current.pause();
+    }
+  });
+
+  const SkipSong = (forward = true) => {
+    if (forward) {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp++;
+        if (temp > props.songs.length - 1) {
+          temp = 0;
+        }
+        return temp;
+      });
+    } else {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp--;
+        if (temp < 0) {
+          temp = props.songs.length - 1;
+        }
+        return temp;
+      });
+    }
+  };
+
   return (
     <div className='c-player'>
-      <audio src=''></audio>
+      <audio
+        src={props.songs[props.currentSongIndex].src}
+        ref={audioEl}></audio>
       <h4>Playing now</h4>
-      <PlayerDetails song={props.songs} />
-      <PlayerControls />
+      <PlayerDetails song={props.songs[props.currentSongIndex]} />
+      <PlayerControls
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        SkipSong={SkipSong}
+      />
       <p>
         <strong>Next up:</strong>
-        {props.nextSong.title} by {props.nextSong.artist}
+        {props.songs[props.nextSongIndex].title} by{" "}
+        {props.songs[props.nextSongIndex].artist}
       </p>
     </div>
   );
