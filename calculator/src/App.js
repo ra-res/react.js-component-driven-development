5e47f6970976b4163d55e6dbb73c4d667b0e80eb
ra-res <ra-res@users.noreply.github.com>
2021-02-17 15:28:09 +0000

    finished prototype

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,13 +1,67 @@
 /** @format */
 
+import { Component } from "react";
+import KeyPad from "./Components/KeyPad";
 import Result from "./Components/Result";
+import "./App.css";
 
-function App() {
-  return (
-    <div className='App'>
-      <Result result={12}></Result>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+    };
+  }
+
+  calculate = () => {
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "") + "",
+      });
+    } catch (e) {
+      this.setState({
+        result: "error",
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: "",
+    });
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1),
+    });
+  };
+
+  onClick = (button) => {
+    if (button === "=") {
+      this.calculate();
+    } else if (button === "C") {
+      this.reset();
+    } else if (button === "CE") {
+      this.backspace();
+    } else {
+      this.setState({
+        result: this.state.result + button,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className='calc-body'>
+          <h1> Calculator </h1>
+          <Result result={this.state.result}></Result>
+          <KeyPad onClick={this.onClick}></KeyPad>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
