8e3d57ddb141dc1abe660940d20c46e9c63116df
ra-res <ra-res@users.noreply.github.com>
2021-02-19 23:59:02 +0000

    removed not needed file:

--- 7-reviews/src/App.js
+++ 7-reviews/src/App.js
@@ -1,8 +1,18 @@
 /** @format */
 
-import "./App.css";
+import Review from "./Review";
 
 function App() {
-  return <div className='App'> Hey</div>;
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review></Review>
+      </section>
+    </main>
+  );
 }
 export default App;
--- 7-reviews/src/Review.js
+++ 7-reviews/src/Review.js
@@ -1,7 +1,8 @@
 /** @format */
 import React from "react";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import people from "./data";
 
 export default function Review() {
-  return <div></div>;
+  return <div>review</div>;
 }
