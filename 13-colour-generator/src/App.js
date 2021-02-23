139074f27d03e37b9d7cfc82b38b9a55946e3676
ra-res <ra-res@users.noreply.github.com>
2021-02-23 20:18:07 +0000

    colour picker

--- 13-colour-generator/src/App.js
+++ 13-colour-generator/src/App.js
@@ -1,10 +1,51 @@
 /** @format */
 
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
+import Values from "values.js";
+import SingleColour from "./SingleColour";
 
 function App() {
-  return <div className='App'></div>;
+  const [colour, setColour] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#f15025").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colours = new Values(colour).all(10);
+      setList(colours);
+      setError(false);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      {" "}
+      <section className='container'>
+        <h3>colour generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={colour}
+            onChange={(e) => setColour(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? "error" : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>{" "}
+      <section className='colors'>
+        {list.map((colour, index) => {
+          return <SingleColour key={index} {...colour} index={index} />;
+        })}
+      </section>
+    </>
+  );
 }
 
 export default App;
--- 13-colour-generator/src/SingleColour.js
+++ 13-colour-generator/src/SingleColour.js
@@ -1,7 +1,35 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import rgbToHex from "./utils";
+export default function SingleColour({ rgb, type, weight, index }) {
+  const [alert, setAlert] = useState(false);
 
-export default function SingleColour() {
-  return <div>colour</div>;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+  return (
+    <article
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(rgbToHex(...rgb));
+      }}
+      className={`color`}
+      style={{ backgroundColor: `rgb(${rgb.join(",")})` }}>
+      <p
+        style={{ color: `${type === "tint" ? "black" : "white"}` }}
+        className='percent-value'>
+        {weight}%
+      </p>
+      <p
+        style={{ color: `${type === "tint" ? "black" : "white"}` }}
+        className='color-value'>
+        {rgbToHex(...rgb)}
+      </p>
+      {alert && <p className='alert'> copied to clipboard </p>}
+    </article>
+  );
 }
