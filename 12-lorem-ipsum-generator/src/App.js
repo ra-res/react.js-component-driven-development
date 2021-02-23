d3ac24b03e04d3e749918faadf334aca4d6ce964
ra-res <ra-res@users.noreply.github.com>
2021-02-23 18:08:02 +0000

    ipsum generator

--- 12-lorem-ipsum-generator/src/App.js
+++ 12-lorem-ipsum-generator/src/App.js
@@ -1,9 +1,44 @@
 /** @format */
-
+import React, { useState } from "react";
 import "./App.css";
+import data from "./data";
 
 function App() {
-  return <div className='App'></div>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    } else if (count > data.length) {
+      amount = data.length;
+    }
+    setText(data.splice(0, amount));
+  };
+  return (
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
