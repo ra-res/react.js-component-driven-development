139079d4300268f41679ecbc3b01977bea3a1d2b
ra-res <ra-res@users.noreply.github.com>
2021-02-24 14:22:35 +0000

    refs used

---
+++ 14-navbar/src/Navbar.js
@@ -0,0 +1,51 @@
+/** @format */
+
+import React, { useEffect, useRef, useState } from "react";
+import logo from "./logo.svg";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data.js";
+export default function Navbar() {
+  const [showLinks, setShowLink] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect();
+    console.log(linksHeight);
+  }, [showLinks]);
+
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='a logo' />
+          <button
+            className='nav-toggle'
+            onClick={() => setShowLink(!showLinks)}>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a href={link.url}>{link.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map((curSocial) => {
+            return (
+              <li key={curSocial.id}>
+                <a href={curSocial.url}>{curSocial.icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
